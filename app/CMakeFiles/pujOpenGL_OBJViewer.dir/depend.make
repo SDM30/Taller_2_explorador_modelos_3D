2e8a12eda78264a3982682baf8837676581d16ac
SDM30 <simondiaz@javeriana.edu.co>
2026-09-18 14:21:23 -0500

    eliminar archivos de ejecucion con Cmake

--- app/CMakeFiles/pujOpenGL_OBJViewer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pujOpenGL_OBJViewer.
-# This may be replaced when dependencies are built.
--- lib/pujOpenGL/KeyboardTrackballCamera.h
+++ lib/pujOpenGL/KeyboardTrackballCamera.h
@@ -32,9 +32,6 @@ namespace pujOpenGL
     virtual void _Update( );
 
   protected:
-
-    float last_x {0};
-    float last_y {0};
     float Phi         { 0 };
     float Theta       { 0 };
     float Radius      { 0 };
